364886c126434d7b9eceeaaa739d794219d17290
Vitaly Sokolsky <v.sokolsky@sumatosoft.com>
2020-10-14 17:41:39 +0300

    Add ENV variables

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -5,6 +5,13 @@ class ConfirmationsController < Devise::ConfirmationsController
   private
 
   def after_confirmation_path_for(resource_name, resource)
-    url_for('http://localhost:8080')
+    url_options = Rails.application.config_for(:urls)[:frontend]
+    redirect_url = URI::Generic.build(
+                     host: url_options[:host],
+                     scheme: url_options[:protocol],
+                     port: url_options[:port],
+                   ).to_s
+
+    url_for(redirect_url)
   end
 end
